5bf3ad5631a56fd15b32407660e151e863020f02
Arpit Pathak <pathakarpit1234@gmail.com>
2025-12-19 15:01:44 +0000

    crud app with db

---
+++ test4/crud-app/main.py
@@ -0,0 +1,53 @@
+import models, schemas, crud
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal, Base
+from typing import List
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+#dependency with the DB
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#endpoints
+# 1 create an employee
+@app.post('/employees', response_model=schemas.EmployeeOut)
+def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
+    crud.create_employee(db, employee)
+
+# 2 get all emplyoees
+@app.get('/employees', response_model=List[schemas.EmployeeOut])
+def get_employees(db: Session = Depends(get_db)):
+    return crud.get_employees(db)
+
+
+# 3 get a specific employee
+@app.get('/employees/{emp_id}', response_model=schemas.EmployeeOut)
+def get_employee(emp_id: int, db: Session = Depends(get_db)):
+    employee = crud.get_employee(db, emp_id)
+    if employee is None:
+        raise HTTPException(status_code=404, detail='your Dad Not Found')
+    return employee
+
+# 4 update employee
+@app.put('/employee/{emp_id}', response_model=schemas.EmployeeOut)
+def update_employee(emp_id: int, employee: schemas.EmployeeUpdate, db: Session = Depends(get_db)):
+    db_employee = crud.update_employee(db, emp_id, employee)
+    if employee is None:
+        raise HTTPException(status_code=404, detail='your Mom Not Found')
+    return db_employee
+
+# 5 delete employee
+@app.delete('/emplyoee/{emp_id}')
+def delete_employee(emp_id: int, db: Session = Depends(get_db)):
+    employee = crud.delete_employee(db, emp_id)
+    if employee is None:
+        raise HTTPException(status_code=404, detail='your Girl Not Found')
+    return {'detail': 'Employee Deleted'}
